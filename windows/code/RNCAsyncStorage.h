fa52dd034f8596727c52bb96cfde1d4c04257757
Vladimir Morozov <vmorozov@microsoft.com>
2021-11-24 19:02:29 -0800

    Make m_dbStorage to be a private field

--- windows/code/RNCAsyncStorage.h
+++ windows/code/RNCAsyncStorage.h
@@ -12,33 +12,32 @@ namespace winrt::ReactNativeAsyncStorage::implementation
 {
     REACT_MODULE(RNCAsyncStorage);
     struct RNCAsyncStorage {
-        DBStorage dbStorage;
-
         REACT_METHOD(multiGet)
         void multiGet(std::vector<std::string> &&keys,
                       DBStorage::ResultCallback &&callback) noexcept
         {
-            dbStorage.AddTask<DBStorage::MultiGetTask>(std::move(keys), std::move(callback));
+            m_dbStorage.AddTask<DBStorage::MultiGetTask>(std::move(keys), std::move(callback));
         }
 
         REACT_METHOD(multiSet)
         void multiSet(std::vector<DBStorage::KeyValue> &&keyValues,
                       DBStorage::Callback &&callback) noexcept
         {
-            dbStorage.AddTask<DBStorage::MultiSetTask>(std::move(keyValues), std::move(callback));
+            m_dbStorage.AddTask<DBStorage::MultiSetTask>(std::move(keyValues), std::move(callback));
         }
 
         REACT_METHOD(multiMerge)
-        void multiMerge(std::vector<DBStorage::KeyValue> && keyValues,
-                        DBStorage::Callback && callback) noexcept
+        void multiMerge(std::vector<DBStorage::KeyValue> &&keyValues,
+                        DBStorage::Callback &&callback) noexcept
         {
-            dbStorage.AddTask<DBStorage::MultiMergeTask>(std::move(keyValues), std::move(callback));
+            m_dbStorage.AddTask<DBStorage::MultiMergeTask>(std::move(keyValues),
+                                                           std::move(callback));
         }
 
         REACT_METHOD(multiRemove)
         void multiRemove(std::vector<std::string> &&keys, DBStorage::Callback &&callback) noexcept
         {
-            dbStorage.AddTask<DBStorage::MultiRemoveTask>(std::move(keys), std::move(callback));
+            m_dbStorage.AddTask<DBStorage::MultiRemoveTask>(std::move(keys), std::move(callback));
         }
 
         REACT_METHOD(getAllKeys)
@@ -46,13 +45,16 @@ namespace winrt::ReactNativeAsyncStorage::implementation
             std::function<void(const DBStorage::Error &error, const std::vector<std::string> &keys)>
                 &&callback) noexcept
         {
-            dbStorage.AddTask<DBStorage::GetAllKeysTask>(std::move(callback));
+            m_dbStorage.AddTask<DBStorage::GetAllKeysTask>(std::move(callback));
         }
 
         REACT_METHOD(clear)
         void clear(std::function<void(const DBStorage::Error &error)> &&callback) noexcept
         {
-            dbStorage.AddTask<DBStorage::ClearTask>(std::move(callback));
+            m_dbStorage.AddTask<DBStorage::ClearTask>(std::move(callback));
         }
+
+    private:
+        DBStorage m_dbStorage;
     };
 }  // namespace winrt::ReactNativeAsyncStorage::implementation
